288e7f187c0329446be6068dca67cceb5d29ff50
Mr.Dung <dung13890@gmail.com>
2018-06-17 23:18:05 +0700

    Notification slack

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	_ "fmt"
+	"net/http"
 )
 
 type CLI struct{}
 
 func main() {
-	fmt.Println("Hello!")
-	fmt.Println("String!")
-	fmt.Println("Block chain!")
-	fmt.Println("decrypt chain!")
+	// fmt.Println("Hello!")
+	// fmt.Println("String!")
+	sendToSlack("test bao thuc")
+}
+
+func sendToSlack(content string) (err error) {
+	client := &http.Client{}
+	url := "https://hooks.slack.com/services/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"
+	var jsonStr = []byte(`{"text":"` + content + `"}`)
+
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Add("Content-Type", "application/json")
+
+	_, responseErr := client.Do(req)
+
+	return responseErr
 }
